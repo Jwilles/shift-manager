f18e555b6d9e3bf7f676d4208044f6580545de2e
John <johnwilles@gmail.com>
2017-01-06 16:27:51 -0500

    gitignore added

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+shifts = [];
+
+
+
+
+app.use(express.static('public'));
+
+:
+
+
+
+app.listen(8000, function() {
+	console.log('Started Server');
+});
